0df9e21b25cb211193c89d14e3ba4b0503b5325c
Mustafa <mostafaismael10@hotmail.com>
2023-01-08 10:01:32 +0300

    finalV3

--- kernels.c
+++ kernels.c
@@ -50,8 +50,8 @@ void naive_conv(int dim, pixel *src, pixel *ker, unsigned *dst)
         }
 }
 
-char convolutionMustafaV2_descr[] = "Convolution: Mustafa v6565";
-void convolutionMustafaV2(int dim, pixel *src, pixel *ker, unsigned *dst)
+char convolutionMustafaV6565_descr[] = "Convolution: Mustafa v6565";
+void convolutionMustafaV6565(int dim, pixel *src, pixel *ker, unsigned *dst)
 {
     int i, j, k;
     int limit = dim - 7;
@@ -121,8 +121,8 @@ void convolutionMustafaV2(int dim, pixel *src, pixel *ker, unsigned *dst)
  * convolution - Your current working version of convolution
  * IMPORTANT: This is the version you will be graded on
  */
-char convolutionMustafaVf_descr[] = "Convolution: Mustafa Vf";
-void convolutionMustafaVf(int dim, pixel *src, pixel *ker, unsigned *dst)
+char convolutionMustafaV6666_descr[] = "Convolution: Mustafa V6666";
+void convolutionMustafaV6666(int dim, pixel *src, pixel *ker, unsigned *dst)
 {
 
     int i, j, k;
@@ -197,182 +197,6 @@ void convolutionMustafaVf(int dim, pixel *src, pixel *ker, unsigned *dst)
 
                 dstL[j + 7] += (elem[7].red * k0r) + (elem[7].green * k0g) + (elem[7].blue * k0b) + (elem[8].red * k1r) + (elem[8].green * k1g) + (elem[8].blue * k1b) + (elem[9].red * k2r) + (elem[9].green * k2g) + (elem[9].blue * k2b) +
                                (elem[10].red * k3r) + (elem[10].green * k3g) + (elem[10].blue * k3b) + (elem[11].red * k4r) + (elem[11].green * k4g) + (elem[11].blue * k4b) + (elem[12].red * k5r) + (elem[12].green * k5g) + (elem[12].blue * k5b) + (elem[13].red * k6r) + (elem[13].green * k6g) + (elem[13].blue * k6b) + (elem[14].red * k7r) + (elem[14].green * k7g) + (elem[14].blue * k7b);
-            }
-        }
-        val += dim;
-    }
-}
-
-/*
- * convolution - Your current working version of convolution
- * IMPORTANT: This is the version you will be graded on
- */
-char convolutionMad_descr[] = "Convolution: Mad";
-void convolutionMad(int dim, pixel *src, pixel *ker, unsigned *dst)
-{
-    int i, j, k;
-    int limit = dim - 7;
-    pixel *kerRow = ker;
-    int val = 0;
-    int k0r, k1r, k2r, k3r, k4r, k5r, k6r, k7r;
-    int k0g, k1g, k2g, k3g, k4g, k5g, k6g, k7g;
-    int k0b, k1b, k2b, k3b, k4b, k5b, k6b, k7b;
-
-    int p0r, p1r, p2r, p3r, p4r, p5r, p6r, p7r;
-    int p0g, p1g, p2g, p3g, p4g, p5g, p6g, p7g;
-    int p0b, p1b, p2b, p3b, p4b, p5b, p6b, p7b;
-    for (k = 0; k < 8; k += 1, kerRow += 8)
-    {
-
-        k0r = kerRow[0].red;
-        k0g = kerRow[0].green;
-        k0b = kerRow[0].blue;
-
-        k1r = kerRow[1].red;
-        k1g = kerRow[1].green;
-        k1b = kerRow[1].blue;
-
-        k2r = kerRow[2].red;
-        k2g = kerRow[2].green;
-        k2b = kerRow[2].blue;
-
-        k3r = kerRow[3].red;
-        k3g = kerRow[3].green;
-        k3b = kerRow[3].blue;
-
-        k4r = kerRow[4].red;
-        k4g = kerRow[4].green;
-        k4b = kerRow[4].blue;
-
-        k5r = kerRow[5].red;
-        k5g = kerRow[5].green;
-        k5b = kerRow[5].blue;
-
-        k6r = kerRow[6].red;
-        k6g = kerRow[6].green;
-        k6b = kerRow[6].blue;
-
-        k7r = kerRow[7].red;
-        k7g = kerRow[7].green;
-        k7b = kerRow[7].blue;
-
-        unsigned *dstL = dst;
-        pixel *elem = src + val;
-        for (i = k; i < limit + k; i += 1, dstL += dim)
-        {
-            p1r = elem[0].red;
-            p1g = elem[0].green;
-            p1b = elem[0].blue;
-
-            p2r = elem[1].red;
-            p2g = elem[1].green;
-            p2b = elem[1].blue;
-
-            p3r = elem[2].red;
-            p3g = elem[2].green;
-            p3b = elem[2].blue;
-
-            p4r = elem[3].red;
-            p4g = elem[3].green;
-            p4b = elem[3].blue;
-
-            p5r = elem[4].red;
-            p5g = elem[4].green;
-            p5b = elem[4].blue;
-
-            p6r = elem[5].red;
-            p6g = elem[5].green;
-            p6b = elem[5].blue;
-
-            p7r = elem[6].red;
-            p7g = elem[6].green;
-            p7b = elem[6].blue;
-
-            for (j = 0; j < limit; j += 8, elem += 8)
-            {
-                p0r = elem[0].red;
-                p0g = elem[0].green;
-                p0b = elem[0].blue;
-
-                p1r = elem[1].red;
-                p1g = elem[1].green;
-                p1b = elem[1].blue;
-
-                p2r = elem[2].red;
-                p2g = elem[2].green;
-                p2b = elem[2].blue;
-
-                p3r = elem[3].red;
-                p3g = elem[3].green;
-                p3b = elem[3].blue;
-
-                p4r = elem[4].red;
-                p4g = elem[4].green;
-                p4b = elem[4].blue;
-
-                p5r = elem[5].red;
-                p5g = elem[5].green;
-                p5b = elem[5].blue;
-
-                p6r = elem[6].red;
-                p6g = elem[6].green;
-                p6b = elem[6].blue;
-
-                p7r = elem[7].red;
-                p7g = elem[7].green;
-                p7b = elem[7].blue;
-
-                dstL[j] += (p0r * k0r) + (p0g * k0g) + (p0b * k0b) + (p1r * k1r) + (p1g * k1g) + (p1b * k1b) + (p2r * k2r) + (p2g * k2g) + (p2b * k2b) +
-                           (p3r * k3r) + (p3g * k3g) + (p3b * k3b) + (p4r * k4r) + (p4g * k4g) + (p4b * k4b) + (p5r * k5r) + (p5g * k5g) + (p5b * k5b) + (p6r * k6r) + (p6g * k6g) + (p6b * k6b) + (p7r * k7r) + (p7g * k7g) + (p7b * k7b);
-
-                p0r = elem[8].red;
-                p0g = elem[8].green;
-                p0b = elem[8].blue;
-
-                dstL[j + 1] += (p1r * k0r) + (p1g * k0g) + (p1b * k0b) + (p2r * k1r) + (p2g * k1g) + (p2b * k1b) + (p3r * k2r) + (p3g * k2g) + (p3b * k2b) +
-                               (p4r * k3r) + (p4g * k3g) + (p4b * k3b) + (p5r * k4r) + (p5g * k4g) + (p5b * k4b) + (p6r * k5r) + (p6g * k5g) + (p6b * k5b) + (p7r * k6r) + (p7g * k6g) + (p7b * k6b) + (p0r * k7r) + (p0g * k7g) + (p0b * k7b);
-
-                p1r = elem[9].red;
-                p1g = elem[9].green;
-                p1b = elem[9].blue;
-
-                dstL[j + 2] += (p2r * k0r) + (p2g * k0g) + (p2b * k0b) + (p3r * k1r) + (p3g * k1g) + (p3b * k1b) + (p4r * k2r) + (p4g * k2g) + (p4b * k2b) +
-                               (p5r * k3r) + (p5g * k3g) + (p5b * k3b) + (p6r * k4r) + (p6g * k4g) + (p6b * k4b) + (p7r * k5r) + (p7g * k5g) + (p7b * k5b) + (p0r * k6r) + (p0g * k6g) + (p0b * k6b) + (p1r * k7r) + (p1g * k7g) + (p1b * k7b);
-
-                p2r = elem[10].red;
-                p2g = elem[10].green;
-                p2b = elem[10].blue;
-
-                dstL[j + 3] += (p3r * k0r) + (p3g * k0g) + (p3b * k0b) + (p4r * k1r) + (p4g * k1g) + (p4b * k1b) + (p5r * k2r) + (p5g * k2g) + (p5b * k2b) +
-                               (p6r * k3r) + (p6g * k3g) + (p6b * k3b) + (p7r * k4r) + (p7g * k4g) + (p7b * k4b) + (p0r * k5r) + (p0g * k5g) + (p0b * k5b) + (p1r * k6r) + (p1g * k6g) + (p1b * k6b) + (p2r * k7r) + (p2g * k7g) + (p2b * k7b);
-
-                p3r = elem[11].red;
-                p3g = elem[11].green;
-                p3b = elem[11].blue;
-
-                dstL[j + 4] += (p4r * k0r) + (p4g * k0g) + (p4b * k0b) + (p5r * k1r) + (p5g * k1g) + (p5b * k1b) + (p6r * k2r) + (p6g * k2g) + (p6b * k2b) +
-                               (p7r * k3r) + (p7g * k3g) + (p7b * k3b) + (p0r * k4r) + (p0g * k4g) + (p0b * k4b) + (p1r * k5r) + (p1g * k5g) + (p1b * k5b) + (p2r * k6r) + (p2g * k6g) + (p2b * k6b) + (p3r * k7r) + (p3g * k7g) + (p3b * k7b);
-
-                p4r = elem[12].red;
-                p4g = elem[12].green;
-                p4b = elem[12].blue;
-
-                dstL[j + 5] += (p5r * k0r) + (p5g * k0g) + (p5b * k0b) + (p6r * k1r) + (p6g * k1g) + (p6b * k1b) + (p7r * k2r) + (p7g * k2g) + (p7b * k2b) +
-                               (p0r * k3r) + (p0g * k3g) + (p0b * k3b) + (p1r * k4r) + (p1g * k4g) + (p1b * k4b) + (p2r * k5r) + (p2g * k5g) + (p2b * k5b) + (p3r * k6r) + (p3g * k6g) + (p3b * k6b) + (p4r * k7r) + (p4g * k7g) + (p4b * k7b);
-
-                p5r = elem[13].red;
-                p5g = elem[13].green;
-                p5b = elem[13].blue;
-
-                dstL[j + 6] += (p6r * k0r) + (p6g * k0g) + (p6b * k0b) + (p7r * k1r) + (p7g * k1g) + (p7b * k1b) + (p0r * k2r) + (p0g * k2g) + (p0b * k2b) +
-                               (p1r * k3r) + (p1g * k3g) + (p1b * k3b) + (p2r * k4r) + (p2g * k4g) + (p2b * k4b) + (p3r * k5r) + (p3g * k5g) + (p3b * k5b) + (p4r * k6r) + (p4g * k6g) + (p4b * k6b) + (p5r * k7r) + (p5g * k7g) + (p5b * k7b);
-
-                p6r = elem[14].red;
-                p6g = elem[14].green;
-                p6b = elem[14].blue;
-
-                dstL[j + 7] += (p7r * k0r) + (p7g * k0g) + (p7b * k0b) + (p0r * k1r) + (p0g * k1g) + (p0b * k1b) + (p1r * k2r) + (p1g * k2g) + (p1b * k2b) +
-                               (p2r * k3r) + (p2g * k3g) + (p2b * k3b) + (p3r * k4r) + (p3g * k4g) + (p3b * k4b) + (p4r * k5r) + (p4g * k5g) + (p4b * k5b) + (p5r * k6r) + (p5g * k6g) + (p5b * k6b) + (p6r * k7r) + (p6g * k7g) + (p6b * k7b);
             }
         }
         val += dim;
@@ -530,8 +354,8 @@ void register_conv_functions()
 {
     add_conv_function(&naive_conv, naive_conv_descr);
     add_conv_function(&convolution, convolution_descr);
-    add_conv_function(&convolutionMustafaV2, convolutionMustafaV2_descr);
-    add_conv_function(&convolutionMustafaVf, convolutionMustafaVf_descr);
+    add_conv_function(&convolutionMustafaV6565, convolutionMustafaV6565_descr);
+    add_conv_function(&convolutionMustafaV6666, convolutionMustafaV6666_descr);
 
     /* ... Register additional test functions here */
 }
